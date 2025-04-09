8ee8d831865bc5f920b0a32256a4f80cc008bd4f
louchengxiang <louchengxiang@erayt.com>
2025-04-09 15:35:07 +0800

    remove github token

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 class Config:
     def __init__(self):
         self.load_config()
@@ -7,7 +7,8 @@ class Config:
     def load_config(self):
         with open('config.json', 'r') as f:
             config = json.load(f)
-            self.github_token = config.get('github_token')
+            token = os.environ.get('github_token')
+            self.github_token = token
             self.notification_settings = config.get('notification_settings')
             self.subscriptions_file = config.get('subscriptions_file')
             self.update_interval = config.get('update_interval', 24 * 60 * 60)  # Default to 24 hours
